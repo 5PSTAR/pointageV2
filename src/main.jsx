62e918e1d6749e4a852fc3f9175440308e874edd
lballo <laura.ballo1993@gmail.com>
2026-09-12 17:34:39 +0200

    suppress

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'));
-}
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
